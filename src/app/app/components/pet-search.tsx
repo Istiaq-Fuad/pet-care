2ec90a4ae14891fce4940859c0fced3beda1a44c
istiaqfuad <istiaqurrahmanfuad@gmail.com>
2024-08-30 01:57:43 +0600

    prevent the search form to reload the page after submitting

--- src/app/app/components/pet-search.tsx
+++ src/app/app/components/pet-search.tsx
@@ -6,14 +6,16 @@ function PetSearch() {
   const { searchQuery, setSearchQuery } = usePetStore();
 
   return (
-    <form className="w-full h-full row-start-1 row-span-1 col-start-1 col-span-1">
+    <form
+      className="w-full h-full row-start-1 row-span-1 col-start-1 col-span-1"
+      onSubmit={(e) => e.preventDefault()}
+    >
       <input
         value={searchQuery}
         placeholder="Search pet"
         onChange={(e) => setSearchQuery(e.target.value)}
         type="text"
         className="w-full h-full rounded-md bg-white/20 outline-none p-3 text-sm placeholder:text-white/50"
-
       />
     </form>
   );
